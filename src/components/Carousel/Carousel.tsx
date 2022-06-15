71911b40880d08836a0dcf109a3bfdf7c3649d9e
kirillEvenstar <kirill.ch@theevenstar.net>
2022-06-15 12:15:28 +0300

    codestyle fixes v2

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import { ReactComponent as ArrowLeft } from './../../assets/ArrowLeft.svg';
-import { ReactComponent as FullScreen } from './../../assets/FullScreen.svg';
+import { ReactComponent as ArrowLeft } from "./../../assets/ArrowLeft.svg";
+import { ReactComponent as FullScreen } from "./../../assets/FullScreen.svg";
 
-import styles from './Carousel.module.scss';
+import styles from "./Carousel.module.scss";
 
 interface IProps {
   next: () => void;
@@ -24,24 +24,24 @@ const Carousel = ({
   page,
   customStyles,
   customArrowLeftStyles,
-  customArrowRightStyles,
+  customArrowRightStyles
 }: IProps) => (
-  <>
-    <div className={customStyles || styles.carousel}>
-      <ArrowLeft
-        className={customArrowLeftStyles || styles.arrow}
-        onClick={prev}
-      />
-      {page}/6
-      <ArrowLeft
-        className={customArrowRightStyles || styles.arrow_right}
-        onClick={next}
-      />
-      {withFullScreen ? (
-        <FullScreen onClick={onOpen} className={styles.full} />
-      ) : null}
-    </div>
-  </>
+
+  <div className={customStyles || styles.carousel}>
+    <ArrowLeft
+      className={customArrowLeftStyles || styles.arrow}
+      onClick={prev}
+    />
+    {page}/6
+    <ArrowLeft
+      className={customArrowRightStyles || styles.arrow_right}
+      onClick={next}
+    />
+    {withFullScreen ? (
+      <FullScreen onClick={onOpen} className={styles.full} />
+    ) : null}
+  </div>
+
 );
 
 export default Carousel;
